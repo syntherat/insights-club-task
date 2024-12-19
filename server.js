dc61ee7cd31226609a1347eb9b7555208b4347b5
Sounak Pal <sounakpal06@gmail.com>
2024-12-19 10:10:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("/index.html");
+  res.sendFile("/index.html");
 });
 
 app.get('/login', (req, res) => {
-  res.send("/public/login.html");
+  res.sendFile("./public/login.html");
 });
 
 app.get('/register', (req, res) => {
-    res.send("/public/register.html");
+    res.sendFile("/public/register.html");
 })
 
 app.listen(port, () => {
